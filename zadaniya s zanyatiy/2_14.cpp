048d6c786e126d21bef79a2362e11ac3ee1146e7
AlexandrRusinov <91492729+AlexandrRusinov@users.noreply.github.com>
2021-12-07 02:49:20 +0500

    Add files via upload

---
+++ zadaniya s zanyatiy/2_14.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    int sum = 0;
+    cin >> n;
+    while (n > 0)
+    {
+        sum += (n % 10);
+        n = n / 10;
+    }
+    cout << sum << endl;
+    return 0;
+}
