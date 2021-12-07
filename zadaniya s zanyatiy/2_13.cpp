ea0cf6a033582e916002aa8ec8a23fa82890f5d5
AlexandrRusinov <91492729+AlexandrRusinov@users.noreply.github.com>
2021-12-07 08:53:54 +0500

    Add files via upload

---
+++ zadaniya s zanyatiy/2_13.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, c, d, x1, x2;
+	cout << "Введите коэффиценты псоледовательно " << endl;
+	cin >> a >> b >> c;
+	d = sqrt(b * b - 4 * a * c);
+	x1 = (-b + d) / (2 * a);
+	x2 = (-b - d) / (2 * a);
+	cout << x1 << x2 << endl;
+	return 0;
+}
+
---
+++ zadaniya s zanyatiy/2_15.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, z, k;
+	cin >> n >> z;
+	bool flag = false;
+	while (n != 0 && !flag)
+	{
+		k = n % 10;
+		if (k == z)
+		{
+			flag = true;
+			cout << "Число присутствует в последовательности";
+		}
+		else
+		{
+			n = n / 10;
+		}
+	}
+	if (n == 0)
+	{
+		cout << "Число не присутствует в последовательности" << endl;
+	}
+	return 0;
+}
