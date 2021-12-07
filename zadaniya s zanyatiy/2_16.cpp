a1f0d8d4a0d094e56b62cc00758c1317db72ff17
AlexandrRusinov <91492729+AlexandrRusinov@users.noreply.github.com>
2021-12-07 09:25:21 +0500

    Add files via upload

---
+++ zadaniya s zanyatiy/2_16.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	int a = -1;
+	int b = 2;
+	int sum = a + b;
+	cin >> n;
+	for(int i=1;i<n; i++)
+	{
+		a = abs(a) + 2;
+		b = b + 2;
+		sum = sum + (a + b);
+	}
+	cout << sum << endl;
+	return 0;
+}
+
