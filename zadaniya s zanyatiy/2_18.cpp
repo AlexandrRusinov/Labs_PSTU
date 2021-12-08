bcbcf7de09a894a865313c876645991593fd7b34
AlexandrRusinov <91492729+AlexandrRusinov@users.noreply.github.com>
2021-12-08 22:19:00 +0500

    Add files via upload

---
+++ zadaniya s zanyatiy/2_18.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int k=0;
+	cin >> n;
+	int nn = n;
+	while (n != 0)
+	{
+		k += 1;
+		n = n / 10;
+
+	}
+	for (int i = k; i!=0; i--)
+	{
+		k--;
+		cout << nn % 10;
+		nn = nn / 10;
+	}
+}
