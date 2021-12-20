01f140b51f4afe7b81a1f524dcb401e1bf576b5c
AlexandrRusinov <91492729+AlexandrRusinov@users.noreply.github.com>
2021-12-21 01:13:52 +0500

    Update metodnewtona.cpp

--- zadaniya s zanyatiy/metodnewtona.cpp
+++ zadaniya s zanyatiy/metodnewtona.cpp
@@ -1,27 +1,25 @@
-﻿// f(x) = x^3 - x - 1
+// f(x) = x^3 - x - 1
 // f`(x) = 3x^2 - 1
 // f``(x) = 6x
 // [1,2]
-//                               МЕТОД ПОЛОВИННОГО ДЕЛЕНИЯ (НЬЮТОНА)
-// f(b) * f``(b) > 0
-// 8 - 2 - 1 * 6*2 > 0 -> x0 = b;
-// x = 2
+//                               МЕТОД ПОЛОВИННОГО ДЕЛЕНИЯ(Ньютона)
 #include <iostream>
 #include <cmath>
 using namespace std;
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	float x,xPred;
-	x = 2;
-	xPred = 0;
-	double eps = 0.000001;
-	
-	while (abs(x - xPred) > eps)
-	{
-		xPred = 0;
-		x = xPred - ((pow(xPred, 3) - xPred - 1) / (3 * pow(xPred, 2) - 1));
-	}
-	cout << "Корни:" << x << ";" << xPred << endl; // xPrev для демонстрации, а так без него
-	return 0;
+    float x, xPrev;
+    // f(b) * f''(b) > 0
+    // 8 - 2 - 1 * 6*2 > 0 => x0 = b;
+    x = 2;
+    xPrev = 0;
+    double eps = 0.000001;
+
+    while (abs(x - xPrev) > eps)
+    {
+        xPrev = x;
+        x = xPrev - ((pow(xPrev, 3) - xPrev - 1) / (3 * pow(xPrev, 2) - 1));
+    }
+    cout << "Корень: " << x << endl;
+    return 0;
 }
