3e0e5651d575c76b2e701fca2016dd6686185897
AlexandrRusinov <91492729+AlexandrRusinov@users.noreply.github.com>
2021-12-21 01:15:13 +0500

    Update metoditeraciy.cpp

--- zadaniya s zanyatiy/metoditeraciy.cpp
+++ zadaniya s zanyatiy/metoditeraciy.cpp
@@ -1,31 +1,28 @@
-﻿// f(x) = x^3 - x - 1
-// f`(x) = 3x^2 - 1
-// f``(x) = 6x
-// [1,2]
-//                               МЕТОД ПОЛОВИННОГО ДЕЛЕНИЯ
-// фи(х) - х + Лямбда(x^3 - x - 1)
-// -1/r < Л < 0, если f`(x) > 0
-// 0 < A < 1/r, если f`(x) < 0
-// r = max(|f`(a)|, |f`(b)|)
-// Л = -1/20 = - 0,05
+// f(x) = x^3 - x - 1
+// f'(x) = 3x^2 - 1
+// f''(x) = 6x;
+// [1, 2]
+// ф(x) = x + λ(x^3 - x - 1)        Метод ИТЕРАЦИЙ
+// -1/r < λ < 0, если f'(x) > 0
+// 0 < λ < 1/r, если f'(x) < 0
+// r = max(|f'(a)|, |f'(b)|)
+// λ = -1/20 = -0.05
 #include <iostream>
 #include <cmath>
 using namespace std;
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	float x, xPred;
-	double eps = 0.000001;
-	double Lambda = -0.05;
-	x = 2; // можно выбрать любое х0 из [1,2] - выбранного заранее промежутка
-	xPred = 0; // делаем заведово сильно меньше х, чтобы цикл праивльно сработал
-	while (abs(x - xPred) > eps)
-	{
-		xPred = x; 
-		x =  Lambda * ((pow(xPred, 3) - xPred - 1) + xPred);
-	}
-	cout << "Корни:" << x << ";" << xPred << endl; // xPrev для демонстрации
-	
-	return 0;
+    float x, xPrev;
+    double lambda = -0.05; // -1/20
+    x = 2; // можно выбрать любое x0 из [1, 2] — выбранного заранее промежутка
+    xPrev = 0; //делаем заведомо сильно меньше x, чтобы цикл правильно сработал
+    double eps = 0.000001;
 
+    while (abs(x - xPrev) > eps)
+    {
+        xPrev = x;
+        x = lambda * (pow(xPrev, 3) - xPrev - 1) + xPrev;
+    }
+    cout << "Корень по методу итераций: " << x << endl;
+    return 0;
 }
