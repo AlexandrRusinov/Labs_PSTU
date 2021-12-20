38ce0735a84f2f7b946cc2df615fa4a9a425a571
AlexandrRusinov <91492729+AlexandrRusinov@users.noreply.github.com>
2021-12-21 00:46:05 +0500

    Add files via upload

---
+++ zadaniya s zanyatiy/3_2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;               //сортировка ВЫБОРКОЙ в дисе подробно
+
+int main()
+{
+    int n = 5;
+    int min_id; // индекс минимального элемента
+    int arr[5] = {5, 2, 4, 0, 6};
+    int key;
+    int tmp;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        min_id = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_id])
+            {
+                min_id = j;
+            }
+        }
+        tmp = arr[min_id];
+        arr[min_id] = arr[i];
+        arr[i] = tmp;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
