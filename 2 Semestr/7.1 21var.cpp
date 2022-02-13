ffbe59eab29d31a67a1e23ea26251eb081d45f22
AlexandrRusinov <91492729+AlexandrRusinov@users.noreply.github.com>
2022-02-13 22:30:47 +0500

    Update 7.1 21var.cpp

--- 2 Semestr/7.1 21var.cpp
+++ 2 Semestr/7.1 21var.cpp
@@ -43,7 +43,7 @@ int task(int n, string s1)//  lal lal lal
 }
 int main()
 {
-	string s1 = "lal lal lal";
+	string s1 = "lala lal lal";
 	s1 = s1 + " ";
 	int a[] = { -10, -20, -30, 50, 60 };
 	cout << task(s1.size(), s1) << endl;
